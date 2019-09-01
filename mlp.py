068e5c137002e0bb99de2f4f2bbe195bb33a809f
eduardoscsouza@gmail.com <eduardoscsouza@gmail.com>
2019-09-01 13:59:39 -0300

    Code set functions and predict

--- mlp.py
+++ mlp.py
@@ -3,30 +3,42 @@ import numpy as np
 
 
 #Função logistica, também conhecida como função sigmoidal
-def logistic(x, der):
+def logistic(x, der=False):
 	return 1/(1 + np.exp(-x)) if not der else np.exp(x)/((1 + np.exp(x))**2)
 
 #Função tangente hiperbolica
-def tanh(x, der):
+def tanh(x, der=False):
 	return np.tanh(x)  if not der else  1 - np.tanh(x)**2
 
 #Função ReLU(rectified linear unit)
-def ReLU(x, der):
+def ReLU(x, der=False):
 	return  np.max(0, x) if not der else (1 if (x>0) else 0)
 
 #Função softplus(suavização da ReLU)
-def softplus(x, der):
+def softplus(x, der=False):
 	return  np.log(1 + np.exp(x)) if not der else 1/(1 + np.exp(-x))
 
 #Função linear f(x)=x
-def linear(x, der):
+def linear(x, der=False):
 	return x if not der else 1
+
+
+
+#Função de Loss mean squared error
+def MSE(pred, true, der):
+	return np.sum(np.square(pred-true))/2 if not der else (pred-true)
+
+
+
+# Calcula a acuracia dados a saida predita e a real
+def accuracy(pred, true):		
+	return np.count_nonzero(np.argmax(pred, axis=1) == np.argmax(true, axis=1))/len(pred)
 
 
 
 class MLP:
 	#Gera a MLP
-	def __init__(self, input_size, layers_sizes, actv_funcs=None):
+	def __init__(self, input_size, layers_sizes, actv_funcs=None, loss_func=MSE, metrics=[("Accuracy", get_acc)]):
 		#Define todas as funções de ativação como sendo a logistica caso não definidas
 		if not actv_funcs:
 			actv_funcs = [logistic for _ in range(len(layers_sizes))]
@@ -36,8 +48,10 @@ class MLP:
 		assert all(layers_sizes > 0)
 		assert len(layers_sizes) == len(actv_funcs)
 
-		#Atribuição das funções de ativação
-		self.actv_funcs = actv_funcs
+		#Atribuição das funções e métricas
+		self.set_actv_funcs(actv_funcs)
+		self.set_loss_func(loss_func)
+		self.set_metrics(metrics)
 
 		#Gera as camadas com pesos entre -1 e 1
 		self.layers = [np.random.random_sample((layers_sizes[0], input_size+1))*2.0 - 1]
@@ -48,41 +62,53 @@ class MLP:
 		self.last_nets = [np.empty((layer_size, )) for layer_size in layers_sizes]
 		self.last_ders = [np.zeros(layer.shape) for layer in layers]
 
-	#Faz o forward da MLP
-	def predict(x):
-		#Verificação da entrada
-	  	assert type(x) == np.ndarray
-	  	assert x.shape == (self.layers[0].shape[1]-1, )
+	#Atribuição das funções de ativação
+	def set_actv_funcs(actv_funcs):
+		#Verificação da entrada e atribuição
+		assert all([callable(actv_func) for actv_func in actv_funcs])
+		self.actv_funcs = actv_funcs
 
-	  	#Executar o forward camada por camada
+	#Atribuição da função de loss
+	def set_loss_func(loss_func):
+		#Verificação da entrada e atribuição
+		assert callable(loss_func)
+		self.loss_func = loss_func
+
+	#Atribuição das métricas
+	def set_metrics(metrics):
+		#Verificação da entrada e atribuição
+		assert all([callable(metric[1]) for metric in metrics])
+		self.metrics = metrics
+
+	#Faz o forward da MLP para uma sample
+	def _predict_single(x):
+		#Executar o forward camada por camada
 	  	self.last_input_vects[0][:-1] = x
 	  	for i in range(len(self.layers)):
 	  		self.last_nets[i] = np.sum(self.last_input_vects[i] * self.layers[i], axis=1)
-	  		self.last_input_vects[i+1][:-1] = self.actv_funcs[i](self.last_nets[i])
+	  		self.last_input_vects[i+1][:-1] = self.actv_funcs[i](self.last_nets[i], False)
 
-	  	#Retornar a saída da ultima camada
 	  	return self.last_input_vects[-1][:-1]
+
+	#Faz o forward da MLP para todo o dataset de entrada
+	def predict(x):
+		#Verificação da entrada
+	  	assert type(x) == np.ndarray
+	  	assert len(x.shape) == 2
+	  	assert x.shape[1] == self.layers[0].shape[1]-1
+
+	  	return np.stack([self._predict_single(sample) for sample in x], axis=0)
+
+	def _get_loss():
+
+	def evaluate(x, y):
+
+	def fit
 		
 
 
 
 
-
-
-# Calcula a acuracia da MLP utilizando uma amostra de entrada e suas respectivas saidas esperadas.
-def get_acc(dataset_x, dataset_y, hidden_layer, out_layer):
-	acc = 0
-	for i in range(dataset_x.shape[0]):
-		out = feed_mlp(dataset_x[i], hidden_layer, out_layer)[0]
-		if(np.argmax(out) == np.argmax(dataset_y[i])):
-			acc += 1
-
-	return acc/dataset_x.shape[0]
-
-"""### Função de Ativação
-
-Como função de ativação, g(), foi usada a função logística, por atender os requisitos de ser uma função contínua, estritamente crescente e limitada. No processo de aprendizado é utilizada a derivada da função g(). Ambas são definidas abaixo.
-"""
 
 
 def fit_mlp(dataset_x, dataset_y, hidden_layer, out_layer, thresh=0.01, eta=0.1, max_it=10000):
